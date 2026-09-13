300b52983da47efd40da2896e40cb9a557a6806c
khalilnougha1-dev <khalilnougha1@gmail.com>
2026-09-13 23:56:08 +0100

    Bump to 2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.sahla.gateway"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "2.2"
+        versionCode = 6
+        versionName = "2.3"
     }
 
     signingConfigs {
